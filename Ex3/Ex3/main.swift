e9afe9ed2fbc76185ca256c246f4ecf3bfd3f9da
Ouardi <ouardi@192.168.1.116>
2023-04-15 14:30:09 +0000

    first

---
+++ Ex3/Ex3/main.swift
@@ -0,0 +1,26 @@
+
+import Foundation
+
+//inputs
+print("Entrer N")
+
+var N = Double(readLine()!)!
+
+// Traitements
+
+var resultat = sqrt(N)
+
+var s = sin(N)
+
+// Output
+print("La racine carée de \(N) est \(resultat)")
+
+print("La sinus de \(N) est \(s)")
+
+/*
+    Ex4 :
+        Ecrire un programme suift qui calcule le cosinus et le carré
+        d'un nombre saisi par l'utilisateur
+ 
+ */
+
---
+++ testapp/testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Ouardi on 13/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
